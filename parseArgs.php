a4ed1cbc07334d6123a932a1f9eb42e7e9d30c3b
Patrick Fisher <patrick@pwfisher.com>
2013-03-16 03:20:29 +0000

    parseArgs function initial commit

---
+++ parseArgs.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * parseArgs Command Line Interface (CLI) utility function.
+ * @usage               $args = CommandLine::parseArgs($_SERVER['argv']);
+ * @author              Patrick Fisher <patrick@pwfisher.com>
+ * @see                 github.com/pwfisher/CommandLine.php
+ */
+function parseArgs($argv) {
+    array_shift($argv); $o = array();
+    foreach ($argv as $a) {
+        if (substr($a,0,2) == '--'){ $eq = strpos($a,'=');
+            if ($eq !== false) { $o[substr($a,2,$eq-2)] = substr($a,$eq+1); }
+            else { $k = substr($a,2); if (!isset($o[$k])){ $o[$k] = true; } } }
+        else if (substr($a,0,1) == '-') {
+            if (substr($a,2,1) == '=') { $o[substr($a,1,1)] = substr($a,3); }
+            else { foreach (str_split(substr($a,1)) as $k){ if (!isset($o[$k])){ $o[$k] = true; } } } }
+        else { $o[] = $a; } }
+    return $o;
+}
